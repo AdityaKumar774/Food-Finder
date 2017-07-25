e437f5db6957a8f393c8b146097887a5c7ca2863
Aditya Kumar <aditya.kumar774@gmail.com>
2017-07-25 21:35:35 +0530

    Restaurant List Created

--- main.js
+++ main.js
@@ -19,6 +19,46 @@ foodFinder.controller('loginController', function($scope){
 });
 
 //Main Controller
-foodFinder.controller('mainController', function($scope){
-	console.log('Main Controller')
+foodFinder.service('restaurantService', function(){
+	this.restaurantLists = [
+		{
+			name: 'Indian Accent',
+			location: 'New Delhi',
+			meal_for_two: 'Rs. 5000/-',
+			favourites: 'The Tasting Menu',
+			recommended_for: 'A special occasion, birthday or anniversary'
+		},
+		{
+			name: 'Peshwari',
+			location: 'Mumbai',
+			meal_for_two: 'Rs. 5000/-',
+			favourites: 'Sikandari Raan and Dal Bukhara',
+			recommended_for: 'For your fix of great North-Indian food in a city where it’s fairly elusive'
+		},
+		{
+			name: 'Villa Maya',
+			location: 'Trivandrum',
+			meal_for_two: 'Rs. 4000/-',
+			favourites: 'Kerala Special and Fish Tikka',
+			recommended_for: 'Calm and peace and leaving the chaos of the world outside'
+		},
+		{
+			name: 'Bukhara',
+			location: 'New Delhi',
+			meal_for_two: 'Rs. 7000/-',
+			favourites: 'Dal Bukhara is legendary (albeit creamy and heavy)',
+			recommended_for: 'Taking your International clients for an Indian meal'
+		},
+		{
+			name: 'Pinch of Spice',
+			location: 'Agra',
+			meal_for_two: 'Rs. 1300/-',
+			favourites: 'Punjabi meat',
+			recommended_for: 'A quick meal when in town'
+		}
+	]
+});
+
+foodFinder.controller('mainController', function($scope, restaurantService) {
+	$scope.restaurantLists = restaurantService.restaurantLists;
 });
